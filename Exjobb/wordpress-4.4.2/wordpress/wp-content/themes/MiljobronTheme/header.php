17499184ff78d496798ccc3ec0ab0134230af779
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2016-04-13 16:19:26 +0200

    Added slideshow to header

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
@@ -1,15 +1,19 @@
 <!doctype html >
 
 <html>
-    
-    
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+ <title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<style>
+.mySlides {display:none}
+.w3-left, .w3-right, .w3-badge {cursor:pointer}
+.w3-badge {height:13px;width:13px;padding:0}
+</style>
+<body>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css"> <!-- enables the awesome fade in effec -->
     <head>
-       
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+ 
         <meta charset="utf-8"/>
         <title><?php bloginfo('title')?></title>
         <link rel ="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
@@ -20,16 +24,44 @@
     </header>
      
      
-     <div id ="container">
-         
-         
-         <img class="mySlides" src="img1.jpg" class="img-responsive img-circle" width="500px" height="400px"/>
-        <img class="mySlides" src="img1.jpg"  width="500px" height="400px"/>
-         
-         
-        
-        
-        <a class="w3-btn-floating" onclick="plusDivs(-1)">&#10094;</a>
-        <a class="w3-btn-floating" onclick="plusDivs(+1)">&#10095;</a>
-        
-     </div>
+    
+<div class="w3-content" style="max-width:600px;position:relative">
+
+<img class="mySlides " src="img1.jpg" height="100px" width="600px"/>
+<img class="mySlides " src="img2.png" height="100px" width="600px"/>
+<div class="w3-center w3-section w3-large w3-text-white w3-display-bottomleft" style="width:100%">
+    <div class="w3-left w3-padding-left w3-hover-text-khaki" onclick="plusDivs(-1)">❮</div>
+    <div class="w3-right w3-padding-right w3-hover-text-khaki" onclick="plusDivs(1)">❯</div>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+  </div>
+
+</div>
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+</script>
