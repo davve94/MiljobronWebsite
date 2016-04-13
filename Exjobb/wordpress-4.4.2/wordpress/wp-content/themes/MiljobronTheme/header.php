1c6071b41875b484b1bba659b5566eefcc7c21f1
davve94 <fodavid@student.chalmers.se>
2016-04-13 12:56:07 +0200

    changed back header

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
@@ -1,18 +1,13 @@
 <!doctype html >
 
 <html>
-    <link rel ="stylesheet" href="style.css" />
     <head>
-      <meta charset="utf-8"/> 
-        
-        <link rel ="stylesheet" href="style.css" />
-       
-    </head>
-    <header>
-    <div id ="head" >
-  
-         <p1> Hej alla glada människor</p1> 
-    </div>
+        <meta charset="utf-8"/>
+        <title><?php bloginfo('title')?></title>
+        <link rel ="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
+       <?php wp_head()?>
+     </head>
+   <header>
+        <h1><a href="<?php home_url('/')?>"<?php bloginfo('name')?></a> </h1>
     </header>
-    
-    <div id ="container">
+     <div id ="container">
