034b71a53160e2322428acb98ca34a0c5bc779a4
davve94 <fodavid@student.chalmers.se>
2016-04-14 09:48:44 +0200

    menu added

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/header.php
@@ -24,10 +24,10 @@
    <div id="head">
         <!--<h1><a href="<?php home_url('/')?>"<?php bloginfo('name')?></a> </h1> -->
         <ul>
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">News</a></li>
-  <li><a href="contact.asp">Contact</a></li>
-  <li><a href="about.asp">About</a></li>
+    <li><a href="default.asp">Home</a></li>
+    <li><a href="news.asp">News</a></li>
+    <li><a href="contact.asp">Contact</a></li>
+    <li><a href="about.asp">About</a></li>
 </ul>
     </div>
      <div id="container"> 
