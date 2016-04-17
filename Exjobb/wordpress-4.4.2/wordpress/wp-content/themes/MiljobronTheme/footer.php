59d3824237d580f560064e0ddae9f8779052b50a
davve94 <fodavid@student.chalmers.se>
2016-04-18 00:07:07 +0200

    nyhets brev, forms, for signing up for newsletter

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/footer.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/footer.php
@@ -6,6 +6,12 @@
     <div id="copyright">  
   <p>  <?=date('Y')?> Copyright © Miljöbron</p> <!-- ta bort year? -->
     </div>
+    <div id="newsletter">
+        <p> Nyhets brev </p>
+        <input type="text" class="form-control" placeholder="E-mail">
+        <p></p>
+        <input type="text" class="form-control" placeholder="Fullt namn">
+    </div>
         
     
 </footer>
