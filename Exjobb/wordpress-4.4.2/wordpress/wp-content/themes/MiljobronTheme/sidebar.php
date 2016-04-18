5a9a18c8d0d67eabbe3177994321ad32d341f712
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2016-04-18 16:06:03 +0200

    added instagram widget

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/sidebar.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/sidebar.php
@@ -7,6 +7,9 @@
         <p>
                     Dagens Nyheter!
         </p> 
+        <div>
+        <script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/07110691bf8e4ba6ff23319f1a96203d8da2f008.html" id="lightwidget_07110691bf" name="lightwidget_07110691bf"  scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+        </div>
         
     
     </div>
