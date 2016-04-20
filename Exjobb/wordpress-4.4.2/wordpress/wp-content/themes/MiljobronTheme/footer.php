c6a33a6d909a3019801152164cd294a0deacb45e
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2016-04-20 15:08:33 +0200

    Added bootstrap footer and updated text & images

--- Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/footer.php
+++ Exjobb/wordpress-4.4.2/wordpress/wp-content/themes/MiljobronTheme/footer.php
@@ -1,31 +1,51 @@
 
+<html>
 
-<footer>
-      <div id="contactinfo"> 
-           <p> Miljöbron Göteborg</p>
-           <p> Anders Carlssons Gata 7 </p>
-           <p> 417 55 Göteborg </p>
-           <p> info(a)miljobron.se </p>
-           <p> 031-202 789 </p>
-           <p> Göteborg </P>
-    
-      </div>
-    
-    <div id="copyright">  
-  <p>  <?=date('Y')?> Copyright © Miljöbron</p> <!-- ta bort year? -->
-    </div>
-   
-    <div id="newsletter">
-        <p> Fyll i för nyhetsbrev! </p>
-        <input type="text" class="form-control" placeholder="E-mail">
-        <p></p>
-        <input type="text" class="form-control" placeholder="Fullt namn">
-        
-    </div>
-   
-            <div id="share-buttons"> 
-    
-  <a class="btn btn-social-icon btn-twitter" href="https://twitter.com//miljobron">
+        <!-- 
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+        -->
+
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+                                
+				   <div id="logo">
+                            <img src="wp-content/themes/MiljobronTheme/Images/MBTP.png" height="75px" weight="75px" alt=""/>
+                            </div>
+				
+                            <div id="copyright">   
+                            <p>    <?=date('Y')?>   Copyright Miljöbron   </p>                   
+                            </div>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Anders Carlssons Gata 7</span>417 55, Göteborg</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>031-202 789</p>
+				</div>
+
+				<div>
+					<i id="email" class="fa fa-envelope"></i>
+					<p><a href="mailto:info@miljobron.se">info@miljobron.se</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Om oss</span>
+					Miljöbron är en ideell organisation som genom stöd från aktörer inom offentlig och privat sektor, samt högskola och universitet, kan förmedla den här hjälpen kostnadsfritt till företag och studenter. Läs mer om våra partner under fliken Partner i menyn.
+				</p>
+
+				<div class="footer-icons">
+<a class="btn btn-social-icon btn-twitter" href="https://twitter.com//miljobron">
     <span class="fa fa-twitter"></span>
   </a>
   <a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/miljobron/">
@@ -34,27 +54,12 @@
   <a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com//miljobron">
     <span class="fa fa-facebook"></span>
   </a>
-    <!-- Facebook 
-    
-    <a href="https://www.facebook.com//miljobron" target="_blank">
-        <img class="img-responsive" src="wp-content/themes/MiljobronTheme/Images/fbshare.png" alt="Facebook" />
-    </a>
 
-    <!-- Twitter 
-    <a href="https://twitter.com//miljobron" target="_blank">
-        <img class="img-responsive" src="wp-content/themes/MiljobronTheme/Images/twittershare.png" alt="Twitter" />
-    </a>
-    
-    <a href="https://www.instagram.com/miljobron/" target="_blank">
-        <img class="img-responsive" src="wp-content/themes/MiljobronTheme/Images/instasharebutton.png" alt="Instagram" />
-    </a> -->
+				</div>
 
-    
-   
+			</div>
 
-</div> 
-    
-</footer>
+		</footer>
 
 </div> 
 <!-- add wp_footer() here! when nav menu works!, before /body -->
